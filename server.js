812fd8d55e52c2e0fc0cf8b4de54bedc8d58bf2b
forevermarsh <marshmansur004@gmail.com>
2024-08-28 13:21:55 +0300

    i relive the story

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ app.get("/users", (req, res) => {
 
 app.post("/users", async (req, res) => {
   try {
-    if (!req.body.name || !req.body.password) {
-      return res.status(400).send("Name and password are required");
+    if (!req.body.name || !req.body.password || !req.body.token) {
+      return res.status(400).send("Name, password and token are required");
     }
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = { name: req.body.name, password: hashedPassword };
+    const hashedToken = await bcrypt.hash(req.body.token, 10)
+    const user = { name: req.body.name, password: hashedPassword, token: hashedToken };
     users.push(user);
     res.status(201).send({ message: "User created successfully" });
   } catch (error) {
@@ -27,14 +28,14 @@ app.post("/users", async (req, res) => {
 
 app.post("/users/login", async (req, res) => {
   try {
-    if (!req.body.name || !req.body.password) {
-      return res.status(400).send("Name and password are required");
+    if (!req.body.name || !req.body.token) {
+      return res.status(400).send("Name and token are required");
     }
     const user = users.find((user) => user.name === req.body.name);
     if (!user) {
       return res.status(400).send("User not found");
     }
-    if (await bcrypt.compare(req.body.password, user.password)) {
+    if (await bcrypt.compare(req.body.token, user.token)) {
       res.send({ success: true });
     } else {
       res.send({ success: false });
