40350001a3385052a74507b4f16bc1fbc7c93bab
forevermarsh <marshmansur004@gmail.com>
2024-09-05 18:00:30 +0300

    chihiro

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -9,7 +9,6 @@ app.use(cors());
 
 dotenv.config();
 const port = process.env.PORT || 4000;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGO_URI);
@@ -56,9 +55,92 @@ app.post("/users", async (req, res) => {
       token: hashedToken,
       firstName,
       lastName,
+      rides: [],
     };
     await usersCollection.insertOne(user);
     res.status(201).send({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+app.post("/ride", async (req, res) => {
+  try {
+    const {
+      username,
+      token,
+      rideCategory,
+      pickupLocation,
+      dropoffLocation,
+      rideDate,
+      rideTime,
+    } = req.body;
+
+    if (
+      !username ||
+      !token ||
+      !rideCategory ||
+      !pickupLocation ||
+      !dropoffLocation ||
+      !rideDate ||
+      !rideTime
+    ) {
+      return res.status(400).send("All fields are required");
+    }
+
+    const user = await usersCollection.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    if (!(await bcrypt.compare(token, user.token))) {
+      return res.status(401).send("Invalid token");
+    }
+
+    if (!user.rides) {
+      user.rides = [];
+    }
+
+    user.rides.push({
+      rideCategory,
+      pickupLocation,
+      dropoffLocation,
+      rideDate,
+      rideTime,
+      rideStatus: "booked",
+    });
+
+    await usersCollection.updateOne({ username }, { $set: user });
+
+    res.send({ message: "Ride booked successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+
+app.get("/rides", async (req, res) => {
+  try {
+    const { username, token } = req.query;
+
+    if (!username || !token) {
+      return res.status(400).send("Username and token are required");
+    }
+
+    const user = await usersCollection.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    if (!(await bcrypt.compare(token, user.token))) {
+      return res.status(401).send("Invalid token");
+    }
+
+    res.json(user.rides);
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal Server Error" });
